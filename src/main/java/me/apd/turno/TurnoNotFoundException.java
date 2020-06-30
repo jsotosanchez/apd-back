84cdd5cab2b5911f514eee34c9e3d0b37f5a3c21
Jose Soto <jose@xoolix.com>
2020-06-30 11:30:57 -0300

    fix de message

--- src/main/java/me/apd/turno/TurnoNotFoundException.java
+++ src/main/java/me/apd/turno/TurnoNotFoundException.java
@@ -3,6 +3,6 @@ package me.apd.turno;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "usuario no encontrado")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "turno no encontrado")
 public class TurnoNotFoundException extends RuntimeException {
 }
